e6f75c7b2985110d70215c6248c2500681389f7f
Ryuuuu2714 <hatomugicha4792@gmail.com>
2025-08-21 23:47:28 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,9 +1,11 @@
 // コマンドとレスポンスを定義
 const COMMANDS = {
-  "help": "利用可能なコマンド: help, login, decode, exit",
+  "help": "利用可能なコマンド: help, login, decode, exit, phptp1, photo2",
   "login": "パスワードを入力してください",
   "password123": "認証成功: ようこそ、システムへ",
   "decode": "暗号文: VGhpcyBpcyBhIHRlc3Q=",
-  "exit": "セッションを終了しました。"
-  "git": "https://github.com/"
+  "exit": "セッションを終了しました。",
+  "git": "https://github.com/",
+  "photo1": 2025-07-14_13.22.46.png ,
+  "photo2": "2025-07-14_13.22.46.png"
 };
