3b2d38e765ea0f40a82113061f4dd42ec63d0940
Ryuuuu2714 <hatomugicha4792@gmail.com>
2025-08-21 23:27:19 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,8 @@
+// コマンドとレスポンスを定義
+const COMMANDS = {
+  "help": "利用可能なコマンド: help, login, decode, exit",
+  "login": "パスワードを入力してください",
+  "password123": "認証成功: ようこそ、システムへ",
+  "decode": "暗号文: VGhpcyBpcyBhIHRlc3Q=",
+  "exit": "セッションを終了しました。"
+};
